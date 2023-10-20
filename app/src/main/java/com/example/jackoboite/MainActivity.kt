50ec8101d33cd6be82b0c0cc499dc6c812cca7d8
ciribinn <numaciribino2003@gmail.com>
2023-10-20 10:59:17 +0200

    ça fait du son

--- app/src/main/java/com/example/jackoboite/MainActivity.kt
+++ app/src/main/java/com/example/jackoboite/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.jackoboite
 
+import android.media.AsyncPlayer
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jackoboite.adapter.SoundAdapter
 import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = findViewById<RecyclerView>(R.id.sound_list)
-        list.adapter = SoundAdapter()
+        val listButton = findViewById<RecyclerView>(R.id.sound_list)
+        listButton.adapter = SoundAdapter()
     }
 }
--- app/src/main/java/com/example/jackoboite/adapter/SoundAdapter.kt
+++ app/src/main/java/com/example/jackoboite/adapter/SoundAdapter.kt
@@ -1,17 +1,20 @@
 package com.example.jackoboite.adapter
 
+import android.media.MediaPlayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jackoboite.R
 
 class SoundAdapter: RecyclerView.Adapter<SoundAdapter.SoundHolder>() {
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var view: View
     class SoundHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.sound_name)
-        val image: ImageView = itemView.findViewById(R.id.sound_image)
+        val button : Button = itemView.findViewById(R.id.playButton)
 
     }
 
@@ -21,15 +24,10 @@ class SoundAdapter: RecyclerView.Adapter<SoundAdapter.SoundHolder>() {
     }
 
     override fun onBindViewHolder(holder: SoundHolder, position: Int) {
-        val sound = list.get(position)
-        holder.itemView.tag = sound.id
-
-
-        holder.image.setOnClickListener {
-            val id = view.tag as Int
-
+        mediaPlayer = MediaPlayer.create(this, R.raw.son1)
+        view.setOnClickListener(){
+            mediaPlayer.start()
         }
-        holder.name.text = "Sound"
     }
     override fun getItemCount(): Int {
         return 10
